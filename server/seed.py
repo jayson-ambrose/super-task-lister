d3e0a4e613810bf6119370612a4ebd83aaccd0d7
jayson <jayson.ambrose.87@gmail.com>
2023-11-12 21:35:35 -0700

    fixed bad code

--- server/seed.py
+++ server/seed.py
@@ -1,3 +1,16 @@
 from random import randint, choice as rc
 import datetime
 from faker import Faker
+
+from app import app
+from models import db, User, List, Task
+
+if __name__ == '__main__':
+    fake = Faker()
+    with app.app_context():
+        
+        
+        print('deleting old data...')
+        User.query.delete()
+        Task.query.delete()
+        List.query.delete()
