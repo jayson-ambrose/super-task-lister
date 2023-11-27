4c1ddb01597f9d9329b6faf8cf9507f1edca2b07
jayson-ambrose <jayson.ambrose.87@gmail.com>
2023-11-27 15:53:34 -0500

    made credentials into form

--- client/src/components/Credentials.jsx
+++ client/src/components/Credentials.jsx
@@ -1,16 +1,24 @@
-import React from "react";
-import { Input, Button } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Input, Button, Form } from "semantic-ui-react";
 
 export default function Credentials() {
 
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
         <div className="mainContainer">
-            <Input placeholder="Username"/>
-            <Input placeholder="Password" type="password"/>
-            <div>
-                <Button primary>Login</Button>
-                <Button primary>Create Profile</Button>
-            </div>
+            <Form onSubmit={console.log(username + ', ' + password)}>
+                <Form.Field onChange= {(e) => setUsername(e.target.value)}>
+                    <label>Username</label>
+                    <Input placeholder ='username' />
+                </Form.Field>
+                <Form.Field onChange={(e) => setPassword(e.target.value)}>
+                    <label>Password</label>
+                    <Input placeholder ='password' type='password' />
+                </Form.Field>
+                <Button type='submit' primary>Login</Button>
+            </Form>
         </div>
     )
 }
